c40937b9e300b6889abfd8873d27dd34eb2cfa94
slim aouadi <Jessermsekni12@gmail.com>
2022-12-11 00:11:04 +0100

    initial project push

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="font-body flex min-h-screen flex-col items-center justify-center py-2 bg-neutral-200" >
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
